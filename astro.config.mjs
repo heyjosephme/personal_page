aeea82e4e987885bd36b22ffaf036679e8fd4769
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2025-02-14 12:50:45 +0900

    feat: import react, tailwindcss and shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
